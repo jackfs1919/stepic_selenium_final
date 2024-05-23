0575f92b3e1b0d9cdbf2490151082f66432ae2f7
jackfs1919 <100045488+jackfs1919@users.noreply.github.com>
2024-05-23 14:41:33 +0300

    change file

--- test_main_page.py
+++ test_main_page.py
@@ -7,3 +7,8 @@ def test_guest_should_see_btn_add_to_buscket(browser):
     button = len(browser.find_elements(*btn))
     assert button > 0, '!!!НЕ НАШЕЛ!!!'
 
+# def test_guest_can_go_to_login_page(browser):
+#     link = "http://selenium1py.pythonanywhere.com/"
+#     browser.get(link)
+#     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
+#     login_link.click()
