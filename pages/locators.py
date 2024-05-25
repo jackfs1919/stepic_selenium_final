14da1b15bf659c1c677f2acc5e46407926637752
Den <New@KOMPUTER.com>
2024-05-25 21:53:44 +0300

    проверка страницы логинв

--- pages/locators.py
+++ pages/locators.py
@@ -1,2 +1,5 @@
 class MainPageLocators():
     LOGIN_LINK = ('css selector', "#login_link")
+class LoginPageLocators():
+    LOGIN_FORM = ('xpath', '//form[@id = "login_form"]')
+    REGISTER_FORM = ('xpath', '//form[@id = "register_form"]')
