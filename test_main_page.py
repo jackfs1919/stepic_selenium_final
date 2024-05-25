a6ca9cc154ee962c75049a3bb8b96183f38eea91
Den <New@KOMPUTER.com>
2024-05-25 17:05:28 +0300

    модификация под PageObject

--- test_main_page.py
+++ test_main_page.py
@@ -1,14 +1,7 @@
-import time
+from .pages.main_page import MainPage
 
-link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-def test_guest_should_see_btn_add_to_buscket(browser):
-    browser.get(link)
-    btn = ('xpath', '//button[contains(@class, "btn-add-to-basket")]')
-    button = len(browser.find_elements(*btn))
-    assert button > 0, '!!!НЕ НАШЕЛ!!!'
-
-# def test_guest_can_go_to_login_page(browser):
-#     link = "http://selenium1py.pythonanywhere.com/"
-#     browser.get(link)
-#     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-#     login_link.click()
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
